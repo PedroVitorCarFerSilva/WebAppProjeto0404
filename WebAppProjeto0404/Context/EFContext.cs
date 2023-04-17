4069df7c3f3c731edaba611207b3a6aa93d374a8
Pedro Vitor Carvalho Ferreira da Silva <carvalho.ferreira@academico.ifrn.edu.br>
2023-04-17 14:37:12 -0300

    confirmar

---
+++ WebAppProjeto0404/Context/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto0404.Models
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categorico> Categoricos { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+
+}
---
+++ WebAppProjeto0404/Models/Fabricante.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto0404.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+        public string Nome { get; set; }
+    }
+}
