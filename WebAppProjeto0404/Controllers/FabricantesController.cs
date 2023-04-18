09e3c2975ca9ef23f2fc23931c692f84228676bf
Pedro Vitor Carvalho Ferreira da Silva <carvalho.ferreira@academico.ifrn.edu.br>
2023-04-18 15:52:12 -0300

    sim

---
+++ WebAppProjeto0404/Controllers/FabricantesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto0404.Models;
+
+namespace WebAppProjeto0404.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(
+                context.Fabricantes.OrderBy(c => c.Nome)
+            );
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
