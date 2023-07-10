6028281ce6fd92414056a6001742add4e2abdbea
Pedro Vitor Carvalho Ferreira da Silva <carvalho.ferreira@academico.ifrn.edu.br>
2023-07-10 14:28:15 -0300

    criacao servicos

---
+++ Servico/Tabelas/CategoricoServico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.Tabelas;
+using Persistencia.DAL.Tabelas;
+
+namespace Servico.Tabelas
+{
+    public class CategoriaServico
+    {
+        private CategoricoDAL categoricoDAL = new CategoricoDAL();
+        public IQueryable<Categorico> ObterCategoricosClassificadasPorNome()
+        {
+            return categoricoDAL.ObterCategoricosClassificadasPorNome();
+        }
+    }
+}
